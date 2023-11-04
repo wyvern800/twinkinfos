9cf2061392f9bc6e19224a0c567f37b154f9ae58
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2023-11-04 19:10:40 -0300

    fix: corrected getAllBuilds route to return everything parsed

--- src/endpoints/builds/builds.controller.ts
+++ src/endpoints/builds/builds.controller.ts
@@ -9,6 +9,10 @@ import BaseResponse from '../../utils/response';
 
 import expressValidator from '../../middlewares/ExpressValidator';
 
+import * as itemsRepository from '../items/items.repository';
+
+import { equipment } from '../../utils/constants';
+
 const routes = Router();
 
 /**
@@ -24,8 +28,78 @@ const routes = Router();
  */
 routes.get('/', async (request: Request, response: Response) => {
   const allBuilds = await repository.getAllBuilds();
-  return BaseResponse.success(response, allBuilds);
+  const parsedBuilds: any[] = [];
+
+  allBuilds.map(build => {
+    // Map all builds and their object key/vals
+    Object.entries(build).map(([buildKey, buildValue]) => {
+      const newBuildObject: any = { ...build };
+
+      if (buildValue !== null) {
+        let newBuildEquipmentParsed: any = {};
+        // if the build key is within the equipments array, then proceed the parsing
+        if (equipment.includes(buildKey)) {
+          Object.entries(buildValue).map(([k, v]) => {
+            // Convert mainItemId to an actual item
+            if (k === 'mainItemId') {
+              const item = itemsRepository.getById(Number(v));
+              if (item) {
+                // newBuildEquipmentParsed.mainItem = item;
+                newBuildEquipmentParsed = { ...item };
+              }
+            } else if (k === 'alternatives') {
+              const parsedAlternactives: any[] = [];
+              const alternativesOriginal: any = v;
+              alternativesOriginal.map((alt: any) => {
+                let newAlternactive: any = {};
+                // Map the alternactive object
+                Object.entries(alt).map(([altKey, altVal]) => {
+                  if (altKey === 'itemId') {
+                    const item = itemsRepository.getById(Number(altVal));
+                    if (item) {
+                      // newAlternactive.item = item;
+                      newAlternactive = { ...newAlternactive, ...item };
+                    }
+                  } else {
+                    newAlternactive.priority = altVal;
+                  }
+                  parsedAlternactives.push(newAlternactive);
+                  return [altKey, altVal];
+                });
+                return newAlternactive;
+              });
+              // Map the alternatives
+              newBuildEquipmentParsed.alternatives = parsedAlternactives;
+            }
+            return [k, v];
+          });
+
+          // delete all old object vals
+          Object.entries(newBuildObject).map(([nBOK, nBOV]) => {
+            if (equipment.includes(nBOK)) {
+              delete newBuildObject[nBOK];
+            }
+            return [nBOK, nBOV];
+          });
+
+          newBuildObject[buildKey] = newBuildEquipmentParsed;
+
+          parsedBuilds.push(newBuildObject);
+        }
+
+        return newBuildEquipmentParsed;
+      }
+      return [buildKey, buildValue];
+    });
+    return build;
+  });
+
+  return BaseResponse.success(response, parsedBuilds);
 });
+
+/* routes.get('/builds/view/:buildId', async (request: Request, response) => {
+
+}); */
 
 // TODO:
 routes.post(
