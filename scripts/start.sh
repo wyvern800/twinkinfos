56ac521fa14118f72f2c50bf224a9bd0d71ad820
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2023-11-09 00:17:15 -0300

    fix: script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find and kill the running process
-pkill -f "pkill -f '/root/api/twinkinfos/node_modules/.bin/ts-node-dev --poll --transpileOnly --ignore-watch node_modules src/server.ts"
+pkill -f "pkill -f 'node'"
 
 # Wait for a few seconds to allow the process to terminate properly
 sleep 5
